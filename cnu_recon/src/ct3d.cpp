5145a43e7a7f89856a3a61f0b0b30617d49d6486
LiuTed <1500012923@pku.edu.cn>
2017-10-29 16:51:55 +0800

    add load_data

--- cnu_recon/src/ct3d.cpp
+++ cnu_recon/src/ct3d.cpp
@@ -1,1 +1,34 @@
 #include "config.h"
+#include <stdio.h>
+
+using ushort = unsigned short;
+
+void load_data(ushort* data, float* ed, int w, int h, int p,
+			   const char* data_name)
+{
+	FILE* input = fopen(data_name, "r");
+	if(!input)
+	{
+		fprintf(stderr, "cannot open data file\n");
+		exit(1);
+	}
+	puts("loading");
+	fseek(input, 1024, SEEK_SET);//ignore header
+	for(int i=0; i<w*h*p; i++)
+	{
+		if(eof(input))
+		{
+			fprintf(stderr, "unexpect ending\n");
+			break;
+		}
+		fscanf("%hd", data+i);
+	}
+	if(ed)
+	{
+		for(int i=0; i<w*h; i++)
+		{
+			fscanf("%f", ed+i);
+		}
+	}
+	fclose(input);
+}
