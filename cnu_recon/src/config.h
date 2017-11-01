a62cdcbfd4d84a048529a899e58bdbfa75d4cf64
Wentai Zhang <rchardx@gmail.com>
2017-11-01 21:40:41 +0800

    minor modification

--- cnu_recon/src/config.h
+++ cnu_recon/src/config.h
@@ -1,4 +1,21 @@
-#ifndef _CONFIG_H_
-#define _CONFIG_H_
+#pragma once
 
-#endif
+extern string table_filename;
+extern string sino_filename;
+extern string image_filename;
+extern string edge_filename;
+extern string init_f_filename;
+extern string init_v_filename;
+
+extern string OUTPUT_DIR;
+
+extern int NX;
+extern int NY;
+extern int NZ;
+extern int NDETECTOR;
+extern int NCHANNEL;
+extern int NPROJ;
+extern int NPROJ_TURN;
+
+extern double SOURCE_TO_ISO;
+extern double SOURCE_TO_DET;
