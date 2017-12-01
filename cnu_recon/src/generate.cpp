c0a405b00bcb17e4b018fb98dc9bd9e112a0e3a3
Wentai Zhang <rchardx@gmail.com>
2017-12-01 05:08:49 +0000

    minor

--- cnu_recon/src/generate.cpp
+++ cnu_recon/src/generate.cpp
@@ -9,9 +9,6 @@ using namespace std;
 typedef unsigned short ushort;
 typedef long long int64;
 
-float img[128*128*128];
-ushort prj[128*128];
-
 inline float sqr(float x) {
     return x*x;
 }
@@ -20,7 +17,7 @@ inline bool equals(float x,int y) {
     return fabs(x-y)<1.25;
 }
 
-void wrapper(int a,int x,int y) {
+void wrapper1(float *img, ushort *prj, int a,int x,int y) {
     float srcX,srcY,srcZ;
     float dstX,dstY,dstZ;
 
@@ -56,7 +53,10 @@ void wrapper(int a,int x,int y) {
     prj[x*128+y] = ushort(sum);
 }
 
-int main(int argc, char** argv) {
+void test1() {
+    float *img = new float[128*128*128];
+    ushort *prj = new ushort[128*128];
+
     for (int k = 0; k<128; ++k) {
         for (int i = 0; i<128; ++i)
             for (int j = 0; j<128; ++j) {
@@ -68,17 +68,17 @@ int main(int argc, char** argv) {
             }
     }
 
-    ofstream pfile("test.txt");
-        for (int ndx = 0; ndx<128; ++ndx) {
-            for (int ndy = 0; ndy<128; ++ndy) {
-                wrapper(0,ndx,ndy);
-                pfile<<prj[ndx*128+ndy]<<' ';
-            }
-            pfile<<endl;
+    //ofstream pfile("test.txt");
+    for (int ndx = 0; ndx<128; ++ndx) {
+        for (int ndy = 0; ndy<128; ++ndy) {
+            wrapper1(img,prj,0,ndx,ndy);
+            //pfile<<prj[ndx*128+ndy]<<' ';
         }
-    pfile.close();
+        //pfile<<endl;
+    }
+    //pfile.close();
 
-    ofstream fou("test.dr", ios::out | ios::binary);
+    ofstream fou("test_360x128x128_128_parallel.dr", ios::out | ios::binary);
     char empty[1024] = {};
     fou.write(empty,1024);
     int64 size = (int64)128 * 128;
@@ -86,5 +86,16 @@ int main(int argc, char** argv) {
         fou.write((char*)prj, sizeof(ushort) * size);
     fou.close();
 
+    delete [] img;
+    delete [] prj;
+}
+
+void test2() {
+
+}
+
+int main(int argc, char** argv) {
+    test1();
+    test2();
     return 0;
 }
