f7c3017121658a66b5cdc3ea7e7d1759f8e0fa3e
LiuTed <1500012923@pku.edu.cn>
2017-12-02 20:27:37 +0800

    one more interesting model

--- cnu_recon/src/generate.cpp
+++ cnu_recon/src/generate.cpp
@@ -214,9 +214,68 @@ void test3() {
     delete [] prj;
 }
 
+void test4() {
+    float *img = new float[128*128*128];
+    ushort *prj = new ushort[360*128*128];
+
+    for (int k = 0; k<128; ++k) {
+        for (int i = 0; i<128; ++i)
+            for (int j = 0; j<128; ++j) {
+                const float sa = sin((float)k/128*2*PI), ca = cos((float)k/128*2*PI);
+                const float x = (i-64.0)*ca-(j-64.0)*sa, y = (j-64.0)*ca+(i-64.0)*sa;
+                const float dst1 = sqrt(sqr(x+32.0)+sqr(y)),
+                            dst2 = sqrt(sqr(x-32.0)+sqr(y)),
+                            dst3 = sqrt(sqr(x)+sqr(y));
+                if((dst1 < 16) || (dst2 > 16 && dst2 < 32) || (dst1 > 32 && dst2 > 32 && y >= 0 && dst3 < 64))
+                    img[k*128*128+i*128+j] = 0.2;
+                else
+                    img[k*128*128+i*128+j] = 0.0;
+
+            }
+    }
+    //ofstream pfile("test.txt");
+    for(int a = 0; a<360; ++a) {
+        for (int ndx = 0; ndx<128; ++ndx) {
+            for (int ndy = 0; ndy<128; ++ndy) {
+                wrapper1(img,prj+a*128*128,a,ndx,ndy);
+            }
+            //pfile<<prj[ndx*128+ndy]<<' ';
+        }
+        //pfile<<endl;
+    }
+    //pfile.close();
+
+    ofstream fou("test_360x128x128_128_parallel_taiji.dr", ios::binary);
+    char empty[1024] = {};
+    fou.write(empty,1024);
+    int64_t size = (int64)128 * 128;
+    for (int k = 0; k<360; ++k)
+        fou.write((char*)(&prj[k*128*128]), sizeof(ushort) * size);
+    fou.close();
+    for(int z = 0; z < 360; z++)
+    {
+        char buf[256];
+        snprintf(buf, 256, "origin_img_128x128x128/%d", z);
+        fou.open(buf, ios::out);
+        fou.precision(6);
+        for(int x = 0; x < 128; x++)
+        {
+            for(int y = 0; y < 128; y++)
+            {
+                fou << prj[z*128*128+x*128+y] << ' ';
+            }
+            fou << endl;
+        }
+        fou.close();
+    }
+    delete [] img;
+    delete [] prj;
+}
+
 int main(int argc, char** argv) {
     test1();
     test2();
     test3();
+    test4();
     return 0;
 }
